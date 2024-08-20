adbbf18316683171c2f751e7f4992823e77ded94
Pilatte <34147102+pilattebe@users.noreply.github.com>
2024-08-20 17:55:06 +0200

    Update index.js from my phone on master 😂

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ export function loadStyle(json) {
     if (!json || typeof json !== "object") {
         return json;
     }
+    if (Array.isArray(json)) {
+        return json.map(item => loadStyle(item))
+    }
     const result = {};
     for (const key of Object.keys(json)) {
         const className = classes[key];
